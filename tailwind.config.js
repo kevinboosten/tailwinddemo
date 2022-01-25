1bc618e5f7d8ec338c348113b37a3793e0b28b27
Kevin Boosten <kboosten@ilionx.com>
2022-01-25 12:55:44 +0100

    feat: add darkmode and reusable button class

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
-  // boxShadow: ["responsive", "hover", "focus", "active"],
   theme: {
     extend: {},
   },
   plugins: [],
+  darkMode: "class",
 };
